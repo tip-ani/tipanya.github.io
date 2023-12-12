8ad73efb7c3cc649a42961aeb04b3c47c7c0e78f
tip-ani <153052026+tip-ani@users.noreply.github.com>
2023-12-12 10:46:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const elemBtn = document.getElementById('elemButton');
+  elemBtn.addEventListener('click', () => {
+    elemBtn.style.display = 'none';
+    const elem = document.getElementById('elem');
+    elem.style.display = 'block';
+  });
+
+const hsBtn = document.getElementById('hsButton');
+  hsBtn.addEventListener('click', () => {
+    hsBtn.style.display = 'none';
+    const hs = document.getElementById('hs');
+    hs.style.display = 'block';
+  });
+
+const shBtn = document.getElementById('shButton');
+  shBtn.addEventListener('click', () => {
+    shBtn.style.display = 'none';
+    const sh = document.getElementById('sh');
+    sh.style.display = 'block';
+  });
+
+const CBtn = document.getElementById('CButton');
+  CBtn.addEventListener('click', () => {
+    CBtn.style.display = 'none';
+    const C = document.getElementById('college');
+    college.style.display = 'block';
+  });
+
+const Btn = document.getElementById('Button');
+  Btn.addEventListener('click', () => {
+    Btn.style.display = 'none';
+    const AboutMe = document.getElementById('AboutMe');
+    AboutMe.style.display = 'block';
+  });
